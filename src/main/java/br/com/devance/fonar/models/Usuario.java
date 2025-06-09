722e7eda75d1afe31b63968801e5374e0cabc304
Antonio <antoniochavesacp@gmail.com>
2025-06-08 22:21:24 -0300

    antonio2

---
+++ src/main/java/br/com/devance/fonar/models/Usuario.java
@@ -0,0 +1,8 @@
+package br.com.devance.fonar.models;
+
+public abstract class Usuario
+
+{
+    private string nome;
+
+}
--- src/test/java/br/com/devance/fonar/models/Usuario.java
+++
@@ -1,8 +0,0 @@
-package br.com.devance.fonar.models;
-
-public class Usuario
-
-{
-
-
-}
