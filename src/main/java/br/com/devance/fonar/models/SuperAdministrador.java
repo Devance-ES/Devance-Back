0a9a0b279817db6731f5a4c7311f640593c0c4b8
Antonio <antoniochavesacp@gmail.com>
2025-06-11 10:10:29 -0300

    Antonio_atualizacao_SuperAdministrador

--- src/main/java/br/com/devance/fonar/models/SuperAdministrador.java
+++ src/main/java/br/com/devance/fonar/models/SuperAdministrador.java
@@ -1,13 +1,37 @@
 package br.com.devance.fonar.models;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SuperAdministrador extends Usuario{
+
+    private String contato;
+    private List<Delegacia> responsavelPor;
+
     public SuperAdministrador() {
     }
 
-    public SuperAdministrador(String nome, String cpf, String email, String senha, LocalDateTime nascimento){
+    public SuperAdministrador(String nome, String cpf, String email, String senha, LocalDateTime nascimento, String contato){
         super (nome,cpf,email, senha, nascimento);
+        this.contato = contato;
+        this.responsavelPor = new ArrayList<>();
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public List<Delegacia> getResponsavelPor() {
+        return responsavelPor;
+    }
+
+    public void setResponsavelPor(List<Delegacia> responsavelPor) {
+        this.responsavelPor = responsavelPor;
     }
 
     public void cadastrarDelegacia(Delegacia delegacia) {
