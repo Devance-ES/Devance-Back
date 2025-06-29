a4d044aebb374f91912777b6574039c2c0874f64
João Pedro <joaopedro.oliveirads@gmail.com>
2025-06-29 14:28:42 -0300

    Correção do erro de } em RepositorioUsuario

--- src/main/java/br/com/devance/fonar/repositorios/RepositorioUsuario.java
+++ src/main/java/br/com/devance/fonar/repositorios/RepositorioUsuario.java
@@ -10,6 +10,10 @@ import java.util.Optional;
 @Repository
 public interface RepositorioUsuario extends JpaRepository<Usuario, Long>, JpaSpecificationExecutor<Usuario> {
     Optional<Usuario> findByCpf(String cpf);
+
     Optional<Usuario> findByEmail(String email);
+
     boolean existsByCpf(String cpf);
+
     boolean existsByEmail(String email);
+}
